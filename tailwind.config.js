61522fa6d88c07ca770b11265b8de3086b976bef
Zach Green <zachjamesgreen@gmail.com>
2021-07-05 16:52:16 -0600

    add webpacker and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './app/**/*.html.erb',
+    './app/**/*.rb'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
