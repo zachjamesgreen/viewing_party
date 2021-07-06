f0539d24a28dbdee6438f21851cc25055af090e7
Zach Green <zachjamesgreen@gmail.com>
2021-07-05 18:53:04 -0600

    add welcome page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      margin: {
+        '1/4': '25%',
+      }
+    },
   },
   variants: {
     extend: {},
