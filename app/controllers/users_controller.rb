4e3517280df89e0da8bc72f25428f5f76eb258f4
NoahZinter <noah.zinter@gmail.com>
2021-07-06 15:30:49 -0600

    Render flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,21 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    user = user_params
+    user[:email] = user[:email].downcase
+    user[:username] = user[:username].downcase
+    new_user = User.new(user)
+    if new_user.save
+      flash[:success] = "Welcome, #{new_user[:username]}!"
+      redirect_to '/'
+    else
+      flash[:error] = "Sorry, account not created! Incorrect/missing info"
+      redirect_to '/register'
+    end
   end
 
   private
   def user_params
-    
+    params.require(:user).permit(:email, :username, :password)
   end
 end
