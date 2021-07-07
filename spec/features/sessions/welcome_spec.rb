f97781bd50050ea8dd30549cdd3045e6984c1e7b
Zach Green <zachjamesgreen@gmail.com>
2021-07-06 19:01:49 -0600

    Add href to test

--- spec/features/sessions/welcome_spec.rb
+++ spec/features/sessions/welcome_spec.rb
@@ -4,14 +4,12 @@ RSpec.describe 'Welcome Page' do
   context 'Has content' do
     it 'has button to login' do
       visit '/'
-      # TODO: Make sure to add link url to test
-      expect(page).to have_link('Login')
+      expect(page).to have_link('Login', href: '/login')
     end
 
     it 'has button to register' do
       visit '/'
-      # TODO: Make sure to add link url to test
-      expect(page).to have_link('Register')
+      expect(page).to have_link('Register', href: '/register')
     end
   end
 end
