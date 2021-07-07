51ff8d5699d503bd915187859065f265bc2c4783
Zach Green <zachjamesgreen@gmail.com>
2021-07-06 19:33:28 -0600

    Finish tests for login page. Implement user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,15 @@ class SessionsController < ApplicationController
 
   def login; end
 
-  def create; end
+  def create
+    user = User.find_by_username params[:username]
+    if user && user.authenticate(params[:password])
+      session['user_id'] = user.id
+      redirect_to dashboard_path
+    else
+      flash[:error] = "Sorry your info is not correct"
+      redirect_to login_path
+    end
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#login'
   post '/login', to: 'sessions#create'
 
-  get '/dashboard', to: 'dashboard#index'
+  get '/dashboard', to: 'dashboard#index', as: 'dashboard'
 
   get '/register', to: 'users#new'
   resources :users
