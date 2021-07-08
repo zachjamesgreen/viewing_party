f8a2ca819529a4b87549f4e70cb6f9be0e9fab45
NoahZinter <noah.zinter@gmail.com>
2021-07-08 12:17:32 -0600

    rubocop refactor

--- app/lib/tmdb_service.rb
+++ app/lib/tmdb_service.rb
@@ -1,9 +1,7 @@
 class TMDBService
-
-
   def self.discover
     url = 'https://api.themoviedb.org/3/discover/movie'
-    res = Faraday.get(url, {api_key: ENV['TMDB_API_KEY']})
+    res = Faraday.get(url, { api_key: ENV['TMDB_API_KEY'] })
     body = JSON.parse(res.body, symbolize_names: true)
     body[:results].map do |movie|
       Movie.new(movie)
