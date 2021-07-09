297ca01098e032f0109b8957b74d7f19a3238f29
Zach Green <zachjamesgreen@gmail.com>
2021-07-08 20:10:38 -0600

    Show a style view parties on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class DashboardController < ApplicationController
   def index
     @user = current_user
+    @my_view_parties = @user.view_parties
+    @attending_view_parties = ViewPartyUser.where(user_id: @user.id).parties
   end
 end
