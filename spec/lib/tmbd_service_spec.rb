a41e715a6b9f6615224d5ceb04fdb9d0a7840cb6
NoahZinter <noah.zinter@gmail.com>
2021-07-09 13:12:07 -0600

    Test movie search api call

--- spec/lib/tmbd_service_spec.rb
+++ spec/lib/tmbd_service_spec.rb
@@ -3,8 +3,12 @@ require 'rails_helper'
 RSpec.describe TMDBService do
   it 'searches' do
     VCR.use_cassette('moviedb_search', :record => :new_episodes) do
-    expect(TMDBService.search('fast')).to eq([])
+      search = TMDBService.search('fast')
+      expect(search[0]).is_a? Movie
+      expect(search[0].title).to eq('Fast & Furious 10')
+      expect(search.length).to eq(20)
+      expect(search.last.title).to eq('Fast Track: No Limits')
     end
-    
+
   end
 end
