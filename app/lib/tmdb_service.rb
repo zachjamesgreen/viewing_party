9609d1be2f5129a6cd3d7d20e6d0460111ad836c
Zach Green <zachjamesgreen@gmail.com>
2021-07-09 20:08:53 -0600

    add 404 page

--- app/lib/tmdb_service.rb
+++ app/lib/tmdb_service.rb
@@ -21,6 +21,10 @@ class TMDBService
     url = "https://api.themoviedb.org/3/movie/#{id}"
     res = Faraday.get(url, { api_key: ENV['TMDB_API_KEY'], append_to_response: 'reviews,credits' })
     body = JSON.parse(res.body, symbolize_names: true)
-    MovieDetail.new(body)
+    if body[:success] == false
+      return nil
+    else
+      MovieDetail.new(body)
+    end
   end
 end
