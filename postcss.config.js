c8d85aa2201e0b47f29124a41a33f7fd80703df0
Zachary Green <zachjamesgreen@gmail.com>
2021-07-12 11:53:53 -0600

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
-    require('tailwindcss')('tailwind.config.js'),
     require('postcss-import'),
+    require('tailwindcss')('tailwind.config.js'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
