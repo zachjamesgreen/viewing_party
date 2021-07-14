eab8954df5fe99eeeb1fbc2219347c337126d2e5
NoahZinter <noah.zinter@gmail.com>
2021-07-14 15:08:25 -0600

    Add content to view_party_mailers

--- app/mailers/view_party_invitation_mailer.rb
+++ app/mailers/view_party_invitation_mailer.rb
@@ -1,8 +1,8 @@
 class ViewPartyInvitationMailer < ApplicationMailer
   def invitation_email
     @view_party = params[:view_party]
+    @host = User.find(@view_party.user_id)
     @recipients = @view_party.view_party_users.map { |vpu| vpu.user }
-    @url = 'heroku.url'
     @recipients.each { |user| invite(user, @view_party) }
   end
 
